9f3f7ecb7a2b96f2a9e916258da176249d626bec
malanagveuday <malangaveuday23@gmail.com>
2019-11-28 22:43:58 +0530

    setting-api: setting routes constraints for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # API DEFINITION
+  namespace :api, default: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    # WE ARE GOING TO LIST OUR RESOURCES HERE
 
+  end
 end
