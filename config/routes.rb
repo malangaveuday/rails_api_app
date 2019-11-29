159b4aaa354049a4b654a426455145c4489a1fb3
malanagveuday <malangaveuday23@gmail.com>
2019-11-29 15:59:29 +0530

    setting-api: set up changes for rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   # API DEFINITION
   namespace :api, default: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    # WE ARE GOING TO LIST OUR RESOURCES HERE
-
+    scope module: :v1,
+          constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
   end
 end
